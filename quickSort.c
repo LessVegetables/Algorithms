66708aa2e8378584f9a47b683d2679d9daaf4636
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2020-12-12 06:21:32 +0700

    Update quickSort.c

--- quickSort.c
+++ quickSort.c
@@ -1,3 +1,10 @@
+//
+//  main.c
+//
+//  Created by Daniel Gehrman
+//  Copyright © 2020 Daniel Gehrman. All rights reserved.
+//
+
 #include <stdio.h>
 
 void swap(int *a, int *b)
@@ -7,67 +14,69 @@ void swap(int *a, int *b)
     *b = temp;
 }
 
+
 void quickSort (int *List[], int Length)
 {
     if (Length > 1)
     {
-      int i = -1, pivot = List[Length - 1];
+      int i = -1, pivot = *List[Length - 1];
 
       for (int j = 0; j < Length - 1; j++)
       {
-          if (List[j] < pivot)
+          if (*List[j] < pivot)
           {
               i++;
-              swap(&List[j], &List[i]);
+              if (*List[i] != *List[j])
+              {
+                  swap(&*List[j], &*List[i]);
+              }
           }
       }
 
-      swap(&List[i + 1], &List[Length - 1]);
-      
+      swap(&*List[i + 1], &*List[Length - 1]);
+        
       printf("%d\n", i);
       int less[i + 1];
-      int greater[Length - i - 2];
+      int greater[Length - i - 1];
 
       for (int a = 0; a <= i + 1; a++)
       {
-          less[a] = List[a];
-          printf ("%d\t", List[a]);
+          less[a] = *List[a];
       }
-      printf("\n");
 
-      for (int a = i + 2; a < Length; a++)
+      for (int a = i + 1; a < Length; a++)
       {
-          greater[a] = List[a];
-          printf ("%d\t", List[a]);
+          greater[a - i - 1] = *List[a];
       }
-      printf("\n");
 
       quickSort (less, i + 1);
-      quickSort (greater, Length - i - 2);
+      quickSort (greater, Length - i - 1);
     }
     
     for (int a = 0; a < Length; a++)
     {
-      printf ("%d\t", List[a]);
+      printf ("%d\t", *List[a]);
     }
-    printf("\n");
+    return;
 }
+
+
+
+
+
 
 
 int main ()
 {
-    /*
 	int Length = 9;
-    int data[Length] = { 1, 3, 7, 2, 5, 9, 6, 8, 4 };
+    int data[9] = { 1, 3, 7, 2, 5, 9, 6, 8, 4 };
     
-    quickSort (data[], Length);
-
+    quickSort (data, Length);
     for (int i = 0; i < Length; i++)
     {
       printf ("%d\t", data[i]);
     }
-	*/
-	
+	/*
 	FILE *input = fopen("input.txt", "r");
 	FILE *output = fopen("output.txt", "w");
 	
@@ -89,7 +98,7 @@ int main ()
     fclose(input);
 
 	// running the algorithm
-    quickSort(data[], Length);
+    quickSort(data, Length);
 
 	// outputting the data into output file
     for (int i = 0; i < Length; i++)
@@ -98,6 +107,6 @@ int main ()
 		fprintf(output, "\n");
     }
     fclose(outputfile);
-
-  return 0;
+    */
+    return 0;
 }
