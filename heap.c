41559a29e9ee86693d06e5faccc7764a29d344d4
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2020-12-16 12:02:09 +0700

    Update heap.c

--- heap.c
+++ heap.c
@@ -6,6 +6,8 @@
 //
 
 #include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
 
 void swap(int *a, int *b)
 {
@@ -30,9 +32,10 @@ void maxHeap(int *data, int *end)
         }
     }
 }
-                            
+
+
 int main() {
-    
+    /*
     int n = 10, data[10] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
     int end = n - 1;
     
@@ -54,6 +57,57 @@ int main() {
         printf("%d\t", data[i]);
     }
     printf("\n");
+    */
+
+    FILE *input = fopen("input.txt", "r");
+
+    // getting data from input file
+    int Length;
+    fscanf(input, "%d", &Length);
+    int data[Length];
+    for (int i = 0; i < Length; i++)
+    {
+        fscanf(input, "%d", &data[i]);
+
+    }
+    fclose(input);
+
+
+    // starting timer
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+
+    // running the algorithm
+    while (end != 0)
+    {
+        maxHeap(&data, &end);
+        swap(&data[0], &data[end]);
+        end--;
+    }
+
+    // ending timer
+    gettimeofday(&stop, NULL);
+    int Time = (stop.tv_usec - start.tv_usec) / 60;
+
+
+    FILE *output = fopen("output.txt", "w");
+    // outputting the data into output file
+    fprintf(output, "It took %d, %d milliseconds\n", Length, Time);
+    fprintf(output, "%d\t", data[0]);
+    for (int i = 1; i < Length; i++)
+    {
+        fprintf(output, "%d\t", data[i]);
+
+        // checker
+        if (data[i - 1] > data[i])
+        {
+            printf("SORTING STOPPED DUE TO UNEXPECTED ERROR\nPLEASE STAY CALM\nREBOOTING COMPUTER\nIF ERROR DOESN'T GO AWAY, PLEASE CONTACT OUR NEAREST INDIAN IT SPECIALIST\n\n");
+            printf("ERROR OCCURRED IN %d AND %d\n", data[i - 1], data[i]);
+            break;
+        }
+    }
+    fprintf(output, "\n");
+    fclose(output);
 
     return 0;
 }
